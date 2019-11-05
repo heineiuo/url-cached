64f8a644e1785bfa12b2371b9fbfe8d86ccb41d3
heineiuo <heineiuo@gmail.com>
2019-11-05 11:08:22 +0800

    0.2.5

--- index.d.ts
+++ index.d.ts
@@ -2,19 +2,20 @@
 // Project: url-cached
 // Definitions by: heineiuo <https://github.com/heineiuo>
 
-import { Readable } from 'stream'
+import { Readable } from "stream";
 
 interface UrlCachedOptions {
-  preferCache?: boolean | false // use cache even expired
-  cacheDir?: string | '~/.url-cached'
-  reload?: boolean | false // true means all old file will be download again
-  expire?: number | 0 // 0 means no expire, only download onece
-  retryInterval?: number | 0 // 0 means no retry, fatal will not download again
+  preferCache?: boolean | false; // use cache even expired
+  cacheDir?: string | "~/.url-cached";
+  reload?: boolean | false; // true means all old file will be download again
+  expire?: number | 0; // 0 means no expire, only download onece
+  retryInterval?: number | 0; // 0 means no retry, fatal will not download again
+  fallbackToIndexHTML?: boolean; // default false; if true, will try to fetch ${ORIGINAL_PATH}/index.html
 }
 
 interface UrlCached {
-  read(url: string, encoding: string): Promise<string>
-  createReadStream(url: string): Readable
+  read(url: string, encoding: string): Promise<string>;
+  createReadStream(url: string): Readable;
 }
 
-export default function (options: UrlCachedOptions): UrlCached
+export default function(options: UrlCachedOptions): UrlCached;
